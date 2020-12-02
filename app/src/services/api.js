03fb6499907fd141574690eb76f351ea6464d0b8
Thais Moraes <murici.thais@gmail.com>
2020-12-01 21:51:25 -0300

    Adiciona logs nos interceptors das requisições

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -4,4 +4,24 @@ const apiService = axios.create({
   baseURL: 'http://10.0.2.2:3000/',
 });
 
+apiService.interceptors.request.use(
+  (config) => {
+    console.tron.log(
+      `Request ${config.method.toUpperCase()} [${config.url}]`,
+      config.data,
+    );
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+apiService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default apiService;
